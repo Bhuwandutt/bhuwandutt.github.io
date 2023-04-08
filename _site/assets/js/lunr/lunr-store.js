59f8e898afc6ff8bbc7b954b4fbc7607de34d9b8
Bhuwan <bhuwandutt2@gmail.com>
2023-04-07 23:55:25 -0400

    First post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Analyse Public Opinion of Covid Vaccines",
+        "excerpt":"I did this project back in March 2021. I had just started with my Masters in Computer Science program and because of the pandemic the university was online. It was a grim time as India was battling with its second and the deadliest wave of Covid. Vaccines had recently rolled...","categories": [],
+        "tags": [],
+        "url": "/birthday/",
+        "teaser": null
+      }]
