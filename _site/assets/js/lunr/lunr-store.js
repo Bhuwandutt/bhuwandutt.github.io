2c62abe2f8d39dbb83af339414a52df5af19f097
Bhuwan <bhuwandutt2@gmail.com>
2023-05-17 19:41:07 -0400

    Add content to first project

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Analyse Public Opinion of Covid Vaccines",
         "excerpt":"I did this project back in March 2021. I had just started with my Masters in Computer Science program and because of the pandemic the university was online. It was a grim time as India was battling with its second and the deadliest wave of Covid. Vaccines had recently rolled...","categories": [],
         "tags": [],
-        "url": "/birthday/",
+        "url": "/Project1/",
         "teaser": null
       }]
