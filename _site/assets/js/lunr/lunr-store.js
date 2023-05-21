4a4a05d260aa010d75eaae8cb6de087634480f6b
Bhuwan <bhuwandutt2@gmail.com>
2023-05-20 23:31:03 -0400

    More changes to first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Analyse Public Opinion of Covid Vaccines",
-        "excerpt":"I did this project back in March 2021. I had just started with my Masters in Computer Science program and because of the pandemic the university was online. It was a grim time as India was battling with its second and the deadliest wave of Covid. Vaccines had recently rolled...","categories": [],
+        "excerpt":"Background I did this project back in March 2021. I had just started with my Masters in Computer Science program and because of the pandemic the university was online. It was a grim time as India was battling with its second and the deadliest wave of Covid. Vaccines had recently...","categories": [],
         "tags": [],
         "url": "/Project1/",
+        "teaser": null
+      },{
+        "title": "Create XRaY Images using GAN'S",
+        "excerpt":"Introduction Before Dall-E.2, Imagen, Stable Diffusion and all these magical AI artists came into this world, there was an algorithm (that is so intituitive and simple in it’s foundation) lying around that gives comparable performance, if not better than the general purpose LIMs and don’t need so much computation reseources....","categories": [],
+        "tags": [],
+        "url": "/Project2/",
         "teaser": null
       }]
